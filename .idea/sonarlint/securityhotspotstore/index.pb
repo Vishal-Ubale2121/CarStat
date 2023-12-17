c0ba729966f8cea8c3fd861a72184c571db540c7
Vishal Ubale <45795478+Vishal-Ubale2121@users.noreply.github.com>
2023-12-18 00:39:59 +0530

    'initial_commit'

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+C
+CarStat/settings.py,b\3\b37ab9f9f37089777fc853b9c20b0328f04513b1
+G
+Authentication/views.py,5\d\5ddd97e16d9d0e64c953de31f825331fcec95a1f
---
+++ Authentication/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.decorators import login_required
+
+def login_user(request):
+    """
+    This method validate the user and login
+    :param request: Type of request (GET or POST)
+    :return: Login page
+    """
+    if request.method == 'POST':
+        request.session['username'] = request.POST.get('username')
+        password = request.POST.get('password')
+        username = request.session.get('username')
+        request.session['username'] = username
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            context = {
+                'text': 'Invalid Username or Password'
+            }
+            messages.success(request, 'Invalid Username or Password')
+            return render(request, 'login.html', context)
+
+        else:
+            context = {'username': username}
+            login(request, user)
+            return render(request, 'index.html', context)
+    else:
+        context = {}
+        return render(request, 'login.html', context)
+
+
+
+
+@login_required
+def logout_user(request):
+    """
+    This method logout the user
+    :param request: Type of request (GET or POST)
+    :return: Logout page
+    """
+    logout(request)
+    return render(request, 'logout.html')
